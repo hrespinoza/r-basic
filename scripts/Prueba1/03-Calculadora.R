771878a90f7b184a712f5669ddab54d517689bc0
hrespinoza <hr.espinoza.a@gmail.com>
2021-08-17 17:34:16 -0500

    Martes1708

--- scripts/Prueba1/03-Calculadora.R
+++ scripts/Prueba1/03-Calculadora.R
@@ -2,6 +2,8 @@
 2*((3+5)/2)
 725%/%7
 725%%7
+2^3*5
+2^
 pi
 2*pi
 5/0
@@ -11,3 +13,24 @@ log(pi)
 abs(-pi)
 factorial(7)
 factorial(4)
+
+x = (pi^2)/2
+edad <-30
+"Hans Ryan" -> nombre
+HOLA = 5
+hola=5
+
+## Función f(x) = x^3 - (3^x) * sen(x)
+
+f= function(x){x^3 - (3^x) * sin(x)}
+
+f(4)
+f(pi/2)
+
+prod <- function(x,y){
+  
+  x*y
+}
+
+prod(5,7)
+
