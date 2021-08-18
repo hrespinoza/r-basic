73bb71be1912fbd944da4b9dafc2e9434b4c5506
hrespinoza <hr.espinoza.a@gmail.com>
2021-08-18 14:07:19 -0500

    Cambios 18/8

---
+++ scripts/Prueba1/Tarea1/Tarea1.R
@@ -0,0 +1,21 @@
+funPru = function(a,b,c){
+  
+  x = (c-b)/a
+  
+  print(x)
+  
+}
+
+funPru(2,4,0)
+funPru(5,3,0)
+funPru(7,4,18)
+funPru(1,1,1)
+
+
+z <-(3 * exp(1))-pi
+print(z,4)
+
+y <- ((2+3i)^2)/(5+8i)
+
+Mod(y)
+print(Mod(y),4)
